278f9b7797b22ca84791cdfcff1fe9fdace1db09
Rajiv <rajiv.ranjan@above-inc.com>
2014-10-16 06:33:50 +0530

    Mocha test cases

---
+++ User.js
@@ -0,0 +1,3 @@
+var getUser = function(){
+	return "test";
+}
---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+// Sample test
+describe('Sum', function(){
+  describe('#sum 2 numbers', function(){
+    it('should return 4 when add 2+2', function(){
+      chai.assert.equal(4, (2+2));
+    });
+  });
+});
