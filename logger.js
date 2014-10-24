6272e287eabfc5fb05a71f42f7b295ff8459f5fc
Rajiv <rajiv.ranjan@above-inc.com>
2014-10-24 06:42:10 +0530

    Added filename and line number for logs

--- logger.js
+++ logger.js
@@ -5,6 +5,8 @@ winston.add(winston.transports.DailyRotateFile, { filename: './logs/development.
 
 var log = function(level, title, message) {
   message = (message instanceof Array || typeof(message) === 'string') ? message : JSON.stringify(message)
+
+  message = traceCaller(new Error().stack) +" : "+ message;
   if (title) {
     message = title +': '+message;
   }
@@ -14,6 +16,21 @@ var log = function(level, title, message) {
   
   winston.log(level, message);
 }
+
+var traceCaller = function(s) {
+    var n=1;
+    n+=1;
+    var a=s.indexOf('\n',5);
+    while(n--) {
+      a=s.indexOf('\n',a+1);
+      if( a<0 ) { a=s.lastIndexOf('\n',s.length); break;}
+    }
+    b=s.indexOf('\n',a+1); if( b<0 ) b=s.length;
+    a=Math.max(s.lastIndexOf(' ',b), s.lastIndexOf('/',b));
+    b=s.lastIndexOf(':',b);
+    s=s.substring(a+1,b);
+    return s;
+  }
 
 module.exports = function() {
   return {
